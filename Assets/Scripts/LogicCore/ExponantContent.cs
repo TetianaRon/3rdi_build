8f024cef7af61e97da9d2fbbf4814ba4e0a89591
Yuriy Paramonov <yuriy.paramonov@outlook.com>
2022-10-30 07:54:39 -0700

    Add other options]

--- Assets/Scripts/LogicCore/ExponantContent.cs
+++ Assets/Scripts/LogicCore/ExponantContent.cs
@@ -1,20 +1,37 @@
+using System;
 using UnityEngine;
 using UnityEngine.Events;
 
 namespace Assets.Scripts.LogicCore
 {
-    [CreateAssetMenu(fileName = "ExponentContent", menuName = "3rdi/ExponentCont", order = 1)]
-    public class ExponentContent : ScriptableObject 
+   [CreateAssetMenu(fileName = "ExponentContent", menuName = "3rdi/ExponentCont", order = 1)]
+    public class ExponentContent : ScriptableObject
     {
+        public bool IsFavorite;
         public string Name;
         public Texture2D Icons;
-	[TextArea]
+        [TextArea]
         public string Description;
         public UnityAction ObjectAction;
         public Texture2D[] Images;
         public AudioClip[] Tracks;
         public GameObject Prefab;
         public ExponentContent[] RefContent;
+
+        public QuizQuestion Questions;
+
+    }
+    
+
+    [Serializable]
+    public class QuizQuestion{
+
+        public string Name;
+        [TextArea]
+        public string Description; 
+
+        public string[] Options;
+        public int RightAnswer;
 
     }
 }
