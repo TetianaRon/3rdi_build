e6a9c36ff43055529eeafa2a1a04d7fdfb03bcc5
Yuriy Paramonov <yuriy.paramonov@outlook.com>
2022-11-05 12:59:35 +0200

    Made most crutial code

---
+++ Assets/ClickableExponat.cs
@@ -0,0 +1,17 @@
+﻿using Assets.Scripts.LogicCore;
+using UnityEngine;
+
+namespace Assets
+{
+    public class ClickableExponat : MonoBehaviour
+    {
+        [SerializeField]
+        private ExponentContent _content;
+
+        public void Click()
+        {
+            PanelManger.ExponatClicked(_content);
+
+        } 
+    }
+}
--- Assets/PanelManger.cs
+++ Assets/PanelManger.cs
@@ -1,16 +1,57 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using Assets.Scripts.LogicCore;
+using Microsoft.MixedReality.Toolkit.UI;
+using TMPro;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 public class PanelManger : MonoBehaviour
 {
+    [FormerlySerializedAs("_mainPanel")]
     [SerializeField]
-    private GameObject _mainPanel;
+    private GameObject _exponatInfoPanel;
 
-    public void ShowMainPanel()
+    private FollowMeToggle _followMe;
+
+    [SerializeField]
+    [Range(1, 10)]
+    private float _distance;
+
+    [SerializeField]
+    private TextMeshPro _title;
+    [SerializeField]
+    private TextMeshPro _description;
+
+    private static PanelManger _instance;
+
+    private void Awake()
     {
-        Debug.Log($"Method is called");
+        _instance = this;
+        _followMe = GetComponent<FollowMeToggle>();
+        HideExponatInfoPanel();
+    }
 
-        _mainPanel.SetActive(true);
+    public void HideExponatInfoPanel()
+    {
+        _exponatInfoPanel.SetActive(false);
+    }
+
+    public static void ExponatClicked(ExponentContent ec)
+    {
+        _instance.ShowExponatInfoPanel(ec);
+    }
+
+    public void ShowExponatInfoPanel(ExponentContent content=null)
+    {
+        _followMe.AutoFollowDistance = _distance;
+        _followMe.SetFollowMeBehavior(true);
+        if (content != null)
+           _title.text=  content.Name;
+           _description.text = content.Description;
+
+
+        _exponatInfoPanel.SetActive(true);
     }
 }
